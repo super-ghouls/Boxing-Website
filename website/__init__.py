93b5b2928f9595bac4ee671070ca6184cdfb1cd7
Dylan Agyemang <105815142+dylanagyemang@users.noreply.github.com>
2022-08-28 19:44:43 -0400

    main

--- website/__init__.py
+++ website/__init__.py
@@ -19,9 +19,11 @@ def create_app():
     
     from .models import User, Note
     
+    create_database(app)
+    
     return app
 
 def create_database(app):
     if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
+        db.create_all(app = app)
         print('Created Database')
