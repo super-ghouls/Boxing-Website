0ef96eaa2410a2e5067e41d0c2077b6b51dc0c65
dylanagyemang <105815142+dylanagyemang@users.noreply.github.com>
2022-10-20 14:02:33 -0400

    m

--- website/static/scroll.js
+++ website/static/scroll.js
@@ -8,6 +8,19 @@ const observer = new IntersectionObserver((entries) => {
         }
     });
 });
+function toggleNav() {
+    var nav = document.getElementById("subnav");
+    var content = document.getElementById("cont");
+    var head = document.getElementById("hed");
+    var arr = document.getElementById("arr");
+    var tog = document.getElementById("toggler");
+    nav.classList.toggle("hide");
+    content.classList.toggle("exp");
+    head.classList.toggle("exp");
+    arr.classList.toggle("adjustarrow");
+    tog.classList.toggle("tcolor");
+  }
+
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
